8dc81dc3aafa8e9ca26e45bea29f0fa0682c61ff
VulcanCCIT <chuckcondron@chuckcondron.com>
2023-06-20 16:14:04 -0500

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Chuck Condron on 6/20/23.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
